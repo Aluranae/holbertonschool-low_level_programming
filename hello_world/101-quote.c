ddeeaae024ed0d2486b2075d567d849709a52e27
Alurae <10475@holbertonstudents.com>
2025-02-16 14:59:01 +0000

    update 101-quote file

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main- Entry point
- * @void: parameter
- * Return: 1 (success)
- **/
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-    write(stderr, "and that piece of art is useful\" - \
-Dora Korpar, 2015-10-19\n");
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
     return (1);
 }
