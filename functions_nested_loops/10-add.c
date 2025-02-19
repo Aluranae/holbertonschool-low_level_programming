494a79b71ac50339b3abd23761ad6edd87765b7c
Alurae <10475@holbertonstudents.com>
2025-02-19 13:03:51 +0000

    create the 10-add.c file and update the README file

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+/**
+ * add - Adds two integers and returns the result.
+ * @i: The first integer to be added.
+ * @j: The second integer to be added.
+ *
+ * Description: This function takes two integers as input,
+ * adds them together, and returns the sum.
+ *
+ * Return: The sum of `i` and `j`.
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
