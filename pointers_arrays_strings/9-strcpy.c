5e8c091d617b502e7832c80fc00bc4a75d8c051c
Alurae <10475@holbertonstudents.com>
2025-02-26 00:19:55 +0100

    create the 9-strcpy.c file

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_array - prints n elements of an array of integers,
+* followed by a new line.
+* @a: Pointer to the array of integers
+* @n: Number of elements to print
+*
+*
+* Return: void
+*/
+
+
+char *_strcpy(char *dest, char *src)
+{
