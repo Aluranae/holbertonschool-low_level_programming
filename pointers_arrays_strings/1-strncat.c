bce16accd75d1eb135096054cff27865d9ce47dd
Alurae <10475@holbertonstudents.com>
2025-02-27 22:44:03 +0100

    update the 1-strncat.c file

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
 * *_strncat - concatenates two strings.
@@ -12,4 +13,21 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-    while
+	size_t i = 0, j = 0;
+
+	while (dest[i] != '\0')
+		{
+		i++;
+		}
+
+	while (j < n && src[j] != '\0')
+		{
+		dest[i] = src[j];
+		i++;
+		j++;
+		}
+
+		dest[i] = '\0';
+
+	return (dest);
+}
