fb30a7d50de7ab453d597832e6a391923b0a87f6
Alurae <10475@holbertonstudents.com>
2025-02-27 22:04:23 +0100

    Test commot vs code

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* *_strncat - concatenates two strings.
+*
+* @src: copy from
+* @dest: copy to
+* @n: is a mark for bytes from src.
+*
+* Return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+    while
