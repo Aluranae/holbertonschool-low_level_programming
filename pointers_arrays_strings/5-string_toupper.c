3c3fa0cf1036bc7bbbf8b55dc14acf385618a743
Alurae <10475@holbertonstudents.com>
2025-02-28 01:02:00 +0100

    create the 5-main.c & 5-string_toupper.c files

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* string_toupper - Converts lowercase letters to uppercase.
+* @str: The string to modify.
+*
+* Return: Pointer to the modified string.
+*/
+
+char *string_toupper(char *)
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
