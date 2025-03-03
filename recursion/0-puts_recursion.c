f9c583967e215c01b6e240d9472a2898c98883b3
Alurae <10475@holbertonstudents.com>
2025-03-03 23:36:49 +0100

    create the 0-puts_recursion file

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: The string to print
+ *
+ * Description: This function prints a string character by character
+ * using recursion instead of loops.
+ * It stops when it reaches the null terminator.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (num == 0) /*cas de base condition d'arret*/
+		return (0);
+
+
+
+}
