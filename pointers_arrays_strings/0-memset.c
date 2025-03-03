7a1195839291f6357cb5efadd0ac97b0743029b2
Alurae <10475@holbertonstudents.com>
2025-03-03 10:15:19 +0000

    edit & update the 0-memset.c file

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,11 +1,25 @@
 #include "main.h"
 
 /**
-* *_strcat - concatenates two strings.
-*
-* @src: copy from
-* @dest: copy to
-*
-*
-* Return: dest
-*/
+ * _memset - Fills a memory area with a constant byte
+ * @s: Pointer to the memory area to fill
+ * @b: Constant byte to copy
+ * @n: Number of bytes to fill
+ *
+ * Description: This function replaces the first n bytes
+ * of the memory area pointed to by s with the value b.
+ *
+ * Return: Returns a pointer to the modified memory area (s).
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+	s[i] = b;
+	i++;
+	}
+	return (s);
+}
