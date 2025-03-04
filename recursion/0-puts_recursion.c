00c4826624f3c861c2f68c0d6a473dd782dbe988
Alurae <10475@holbertonstudents.com>
2025-03-04 09:57:10 +0000

    edit & update the 0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -11,9 +11,12 @@
 
 void _puts_recursion(char *s)
 {
-	if (num == 0) /*cas de base condition d'arret*/
-		return (0);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
 
-
-
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
