a27c6a50da62a4c73aa9c01d96dd6b48d53bd16b
Alurae <10475@holbertonstudents.com>
2025-03-05 00:47:39 +0100

    create & edit the 4-add.c file

---
+++ argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - Adds positive numbers
+*
+* This program takes multiple arguments, checks if they
+* are valid positive integers, adds them together, and
+* prints the result followed by a new line.
+* If no arguments are provided, it prints 0.
+* If an argument contains a non-digit character,
+* it prints "Error" and returns 1.
+*
+* @argc: Argument count (number of command-line arguments)
+* @argv: Argument vector (array of strings containing the arguments)
+*
+* Return: 0 on success, 1 on error
+*/
+
+int main(int argc, char **argv)
+{
+
+
+
+
+
+
+
+
+}
