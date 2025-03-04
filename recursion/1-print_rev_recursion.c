010c650f958c8af209c178e25267e4f64bd8ecaa
Alurae <10475@holbertonstudents.com>
2025-03-04 11:31:25 +0000

    create & edit the 1-print_rev_recursion.c file

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: The string to print
+ *
+ * Description: This function prints each character of the string
+ * in reverse order using recursion instead of loops.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: The string to measure
+ *
+ * Description: This function calculates the length of a string
+ * using recursion instead of loops.
+ * It stops counting when it reaches the null terminator.
+ *
+ * Return: The length of the string as an integer.
+ */
+
+int _strlen_recursion(char *s)
