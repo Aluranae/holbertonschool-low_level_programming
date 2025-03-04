557f52c6636f33572d4dbf4faad824c35dc09d2e
Alurae <10475@holbertonstudents.com>
2025-03-04 20:19:08 +0100

    edit 0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,15 +2,15 @@
 #include "main.h"
 
 /**
- * main - Prints the program's name
- *
- * This program displays its own name as executed.
- * If the program is renamed, it will print the new name
- * without requiring recompilation.
- * The full path is displayed if executed with one.
- *
- * @argc: Argument count (number of command-line arguments)
- * @argv: Argument vector (array of strings containing the arguments)
- *
- * Return: Always 0 (Success)
- */
+* main - Prints the program's name
+*
+* This program displays its own name as executed.
+* If the program is renamed, it will print the new name
+* without requiring recompilation.
+* The full path is displayed if executed with one.
+*
+* @argc: Argument count (number of command-line arguments)
+* @argv: Argument vector (array of strings containing the arguments)
+*
+* Return: Always 0 (Success)
+*/
