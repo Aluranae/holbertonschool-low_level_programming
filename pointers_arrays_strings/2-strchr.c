18e0921eb9cff39dd40be316c8dd87fcd49adde6
Alurae <10475@holbertonstudents.com>
2025-03-05 08:20:12 +0000

    edit the 2-strchr.c file

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -15,15 +15,14 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
+	while (*s)
 	{
-		if (s[i] == c)
-		return (s + i);
-
-	i++;
+		if (*s == c)
+			return (s);
+	s++;
 	}
+	if (c == '\0')
+		return (s);
 
 	return (NULL);
 }
