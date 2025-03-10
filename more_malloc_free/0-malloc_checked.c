09762c9c726ce6f76126ab98165b7e76821b07fa
Alurae <10475@holbertonstudents.com>
2025-03-10 19:10:37 +0100

    create dir & files

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* malloc_checked - Allocates memory using malloc.
+* @b: The number of bytes to allocate.
+*
+* Return: Pointer to allocated memory.
+* If malloc fails, the function exits with status 98.
+*/
+
+void *malloc_checked(unsigned int b)
+
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
