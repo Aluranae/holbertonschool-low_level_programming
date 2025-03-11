b8a44196556ba6cd0b8625410e5168cd3f38d6a3
Alurae <10475@holbertonstudents.com>
2025-03-11 08:02:23 +0100

    edit the 2-calloc.c file

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,4 +12,25 @@
 */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
-{}
+{
+	void *ptr;
+
+	unsigned int i;
+
+	char *filler;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	filler = ptr;
+
+	for (i = 0; i < (nmemb * size); i++)
+		filler[i] = '\0';
+
+	return (ptr);
+}
