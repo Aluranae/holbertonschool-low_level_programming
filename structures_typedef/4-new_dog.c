b96bcf9eef6428e022af02df5d1b1d1d5cec14f8
Alurae <10475@holbertonstudents.com>
2025-03-12 08:37:23 +0000

    edit & update the 4-new_dog.c file

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,41 +9,42 @@
 * @age: Float value (dog's age)
 * @owner: Pointer to a string (owner's name)
 *
-* Return: Pointer to the new dog
+* Return: Pointer to the new dog, or NULL if allocation fails
 */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	/* Declare a pointer to a new dog */
-        dog_t *new_dog;
+	dog_t *new_dog;
 
 	/* Check if input strings are NULL */
 	if (name == NULL || owner == NULL)
-	return (NULL);
+		return (NULL);
 
 	/* Allocate memory for the new dog */
 	new_dog = malloc(sizeof(dog_t));
 
 	/* Check if the memory allocation was successful */
 	if (new_dog == NULL)
-	return (NULL);
+		return (NULL);
 
-	/* Allocate and copy the dog's name */
-	new_dog->name = strdup(name);
+    	/* Allocate and copy the dog's name */
+	new_dog->name = malloc(strlen(name) + 1);
 	if (new_dog->name == NULL)
 	{
-	free(new_dog);
-	return (NULL);
+		free(new_dog);
+		return (NULL);
 	}
+	strcpy(new_dog->name, name);
 
 	/* Allocate and copy the dog's owner */
-	new_dog->owner = strdup(owner);
+	new_dog->owner = malloc(strlen(owner) + 1);
 	if (new_dog->owner == NULL)
 	{
-	free(new_dog->name);
-	free(new_dog);
-	return (NULL);
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
 	}
+	strcpy(new_dog->owner, owner);
 
 	/* Assign the dog's age */
 	new_dog->age = age;
