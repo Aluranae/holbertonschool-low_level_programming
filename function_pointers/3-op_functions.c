5d2b2c40bb8a404a723a751f6a1ed79f0979099c
Aluranae <10475@holbertonstudents.com>
2025-03-18 07:47:01 +0100

    fix mising code

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -61,6 +61,11 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+			exit(100);
+	}
 	return (a / b);
 }
 
@@ -76,5 +81,10 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+			exit(100);
+	}
 	return (a % b);
 }
