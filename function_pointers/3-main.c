2f217f92faadb285b15bfd60f3f0caafd9bd4b78
Aluranae <10475@holbertonstudents.com>
2025-03-18 07:51:10 +0100

    fixing code

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,44 +12,26 @@
 
 int main(int argc, char *argv[])
 {
-	int (*op_func)(int, int); /* Function pointer to the selected operation */
+	int (*op_func)(int, int);
 	int num1, num2;
 
-	/* Check if the user provided exactly 3 arguments */
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+		if (argc != 4)
+		{
+			printf("Error\n");
+				exit(98);
+		}
 
-	/* Convert the first and third arguments to integers */
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-	/* Validate the operator input */
-	if (argv[2] == NULL || argv[2][0] == '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	/* Retrieve the corresponding function for the operator */
 	op_func = get_op_func(argv[2]);
-	if (op_func == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
 
-	/* Check for division or modulo by zero */
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
+		if (op_func == NULL)
+		{
+			printf("Error\n");
+				exit(99);
+		}
 
-	/* Execute the function and print the result */
 	printf("%d\n", op_func(num1, num2));
-
 	return (0);
 }
