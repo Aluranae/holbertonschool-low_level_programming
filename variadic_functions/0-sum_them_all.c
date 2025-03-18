d44fa1a8c3d3a0eb41401f44f161a427431c734e
Aluranae <10475@holbertonstudents.com>
2025-03-18 13:01:57 +0100

    create firsr files & README file for the project & edit them

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+* sum_them_all - Returns the sum of all given parameters.
+* @n: Number of arguments.
+*
+* Return: Sum of all arguments, or 0 if n is 0.
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+	va_list args;
+
+	int sum = 0;
+
+	unsigned int i;
+
+	va_start(args, n);
+
+	if (n == 0)
+	{
+	return (0);
+	}
+
+	for (i = 0; i < n; i++)
+
+	{
+		sum += va_arg(args, int);
+	}
+
+	va_end(args);
+	return (sum);
+
+}
