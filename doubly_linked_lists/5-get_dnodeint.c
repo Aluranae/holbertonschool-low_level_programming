1a42f600b2a070a821dfc28bf2fc9007862a01cb
Alurae <10475@holbertonstudents.com>
2025-04-01 09:39:29 +0200

    fix: correct variable usage in get_dnodeint_at_index function to iterate through the list

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	dlistint_t *current = head;
 
-	while (head != NULL)
+	while (current != NULL)
 	{
 		if (i == index)
 			return (current);
