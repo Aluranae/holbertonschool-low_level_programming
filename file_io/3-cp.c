c55713abe11030b118c2735c250b700f83a04aa1
Alurae <10475@holbertonstudents.com>
2025-04-01 20:50:05 +0200

    refactor: simplify file copy implementation and improve error handling

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,70 +1,58 @@
 #include "main.h"
-#include <fcntl.h>      /* for open */
-#include <unistd.h>     /* for read, write, close */
-#include <stdlib.h>     /* for malloc, free, exit */
-#include <stdio.h>      /* for dprintf */
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* main - Entry point: copies content from one file to another
-* @ac: Argument count
-* @av: Argument vector
-* Return: 0 on success, exits with codes 97-100 on error
-*/
+ * close_checked - Safely closes a file descriptor
+ * @fd: File descriptor
+ */
+void close_checked(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
 
+/**
+ * main - Copies content from file_from to file_to
+ * @ac: Argument count
+ * @av: Argument vector
+ * Return: 0 on success, exits with codes 97-100 on error
+ */
 int main(int ac, char **av)
 {
 	int fd_from, fd_to;
 	ssize_t r_bytes, w_bytes;
-	char *buffer;
+	char buffer[1024];
 
 	if (ac != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	buffer = malloc(1024);
-	if (buffer == NULL)
-	{
-		dprintf(2, "Error: Can't allocate buffer\n");
-		exit(99);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		free(buffer);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
+
+	while ((r_bytes = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		free(buffer);
-		close(fd_from);
-		exit(99);
+		w_bytes = write(fd_to, buffer, r_bytes);
+		if (fd_to == -1 || w_bytes != r_bytes)
+			close_checked(fd_from),
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]),
+			exit(99);
 	}
 
-	/* === LOOP TO READ/WRITE CONTENT (à compléter) === */
-	/* while ((r_bytes = read(...)) > 0) */
-	/*    w_bytes = write(...); */
-	/*    if (w_bytes != r_bytes) → exit(99) */
-	/* if (r_bytes == -1) → exit(98) */
+	if (r_bytes == -1)
+		close_checked(fd_from),
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]),
+		exit(98);
 
-	free(buffer);
-
-	if (close(fd_from) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	if (close(fd_to) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-
+	close_checked(fd_from);
+	close_checked(fd_to);
 	return (0);
 }
